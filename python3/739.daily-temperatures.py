dc23130b6b34f78e1e3f23ce9c2ed719dde51228
Gpzz <ben.dt.taylor@gmail.com>
2023-06-14 20:46:14 -0300

    added python3 solution to daily-temperatures problem

--- python3/739.daily-temperatures.py
+++ python3/739.daily-temperatures.py
@@ -8,5 +8,20 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         
+        answer = [0] * len(temperatures)
+
+        stack = [] # [index, temperature]
+
+        for index, temperature in enumerate(temperatures):
+
+            while stack and temperature > stack[-1][1]:
+
+                stackindex, stacktemp = stack.pop()
+                answer[stackindex] = index - stackindex
+            
+            stack.append([index, temperature])
+
+        
+        return answer
 # @lc code=end
 
