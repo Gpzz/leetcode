8dfa81d3e8d9acd6523e807e78a2e1917a3a0063
Gpzz <ben.dt.taylor@gmail.com>
2023-06-14 20:07:12 -0300

    added template for python3 solution to daily-temperatures problem

---
+++ python3/739.daily-temperatures.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=739 lang=python3
+#
+# [739] Daily Temperatures
+#
+
+# @lc code=start
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+# @lc code=end
+
