b715556dfbab4935114bddbc40c1c586bf5a57ae
Gpzz <ben.dt.taylor@gmail.com>
2024-02-24 23:30:26 -0400

    added python3 solution to add-two-numbers problem

--- python3/2.add-two-numbers.py
+++ python3/2.add-two-numbers.py
@@ -14,20 +14,28 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
         carry = 0
-        answer = ListNode(0)
-        pointer = answer
+        pointer = res = ListNode(0)
 
+        # While any remaining digits have not yet been added
         while l1 or l2 or carry:
 
-            result = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
-            carry = result // 10
+            result = 0
+
+            if l1:
+                result += l1.val
+                l1 = l1.next
+            if l2:
+                result += l2.val
+                l2 = l2.next
             
+            result += carry
+            # Quotient is the carry variable
+            carry = result // 10
+
+            # Remainder is the current digit of the answer
             pointer.next = ListNode(result % 10)
             pointer = pointer.next
 
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-
-        return answer.next
+        return res.next
 # @lc code=end
 
